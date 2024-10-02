2221740b7b53ab1d26a325a09a61e2d6846af94e
DanielMagero <mdmagero700@gmail.com>
2024-10-02 12:01:06 +0300

    the_start

---
+++ binary_serach.py
@@ -0,0 +1,25 @@
+def binary_serach(array, target):
+  left = 0
+  right = len(array) - 1
+  while left <= right:
+    mid = (left + right)//2
+
+    if array[mid] == target:
+      return mid
+    elif array[mid] < target:
+      left = mid+1
+    else:
+      right=mid-1
+
+  return -1
+
+array = [1,3,5,6,4,7,8,10]
+
+target = 7
+
+result = binary_serach(array, target)
+
+if result != -1:
+  print("Element is at index", result)
+else:
+  print("Element not found")
---
+++ linear_serach.py
@@ -0,0 +1,18 @@
+def linear_search(array, target):
+  for i in range(len(array)):
+    if array[i] == target:
+      return i
+    
+  return -1
+  
+
+data = [2,5,9,1,6]
+target = 6
+
+result = linear_search(data, target)
+print(result)
+
+if result != -1:
+  print("The element is at index", result)
+else:
+  print("The element was not found!")
